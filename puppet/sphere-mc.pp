ee640b0ed6fa2cf3e45107767e0d66e4d2da6a3b
Hajo Eichler <hajo.eichler@commercetools.de>
2013-11-05 17:17:09 +0100

    Add puppet example.

---
+++ puppet/sphere-mc.pp
@@ -0,0 +1,22 @@
+class sphere::mc(
+  $mailjet_api_key=hiera("mailjet.api.key"),
+  $sphere_mc_version=hiera("sphere.mc.version")) {
+
+  package { "sphere-mc":
+    ensure => $sphere_mc_version,
+    notify => File["/etc/sphere-mc/application.conf"]
+  }
+  ->
+  file { "/etc/sphere-mc/application.conf":
+    ensure => present,
+    owner => sphere, group => sphere, mode => 0500,
+    content => template("sphere/application.conf.erb"),
+    notify => Service["sphere-mc"]
+  }
+  ->
+  service { "sphere-mc":
+    ensure => running,
+    enable => true
+  }
+
+}
